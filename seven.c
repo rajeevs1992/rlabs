6dea10defbd2999039d895d5783955372ba3cf16
Rajeev S <rajeevs1992@gmail.com>
2012-02-05 22:39:18 +0530

    seven segment display completed

---
+++ seven.c
@@ -0,0 +1,31 @@
+#include<msp430.h>
+void delay()
+{
+	int i=32000;
+	while(i--);
+}
+main()
+{
+	P1DIR=0x7f;
+	while(1)
+	{
+		P1OUT=0x6;
+		delay();
+		P1OUT=0x5b;
+		delay();
+		P1OUT=0x4f;
+		delay();
+		P1OUT=0x66;
+		delay();
+		P1OUT=0x6d;
+		delay();
+		P1OUT=0x7d;
+		delay();
+		P1OUT=0x7;
+		delay();
+		P1OUT=0x7f;
+		delay();
+		P1OUT=0x67;
+		delay();
+	}
+}
