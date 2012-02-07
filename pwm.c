eff77ddaaa3d52a62cf6c92fad4f064c52397c4b
Rajeev S <rajeevs1992@gmail.com>
2012-02-08 00:25:30 +0530

    completed pwm

---
+++ pwm.c
@@ -0,0 +1,25 @@
+#include<msp430.h>
+#define MAX 1000
+void delay(int i)
+{
+	while(i--);
+}
+main()
+{
+	P1DIR=1;
+	P1OUT=0;
+	int i=0,c=1;
+	while(1)
+	{
+		P1OUT=1;
+		i=i+c;
+		delay(i);
+		P1OUT=0;
+		delay(MAX-i);
+		if(i==MAX)
+			c=-1;
+		else if(i==0)
+			c=1;
+	}
+}
+
---
+++ try.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int i=0;
+	while(1)
+	{
+		printf("%d\t",i%10);
+		i++;
+	}
+}
