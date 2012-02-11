3c3948feca6e986008699203a007209622a06218
Rajeev S <rajeevs1992@gmail.com>
2012-02-11 15:57:26 +0530

    adc modified with an external led

--- adc.c
+++ adc.c
@@ -3,8 +3,8 @@
 void init_adc()
 {
 	ADC10CTL0|=ADC10ON|SREF_0|ADC10SHT_2;
-	ADC10CTL1|=INCH_6|SHS_0|ADC10DIV_0|ADC10SSEL_0|CONSEQ_0;
-	ADC10AE0=BIT6;
+	ADC10CTL1|=INCH_4|SHS_0|ADC10DIV_0|ADC10SSEL_0|CONSEQ_0;
+	ADC10AE0=BIT4;
 	ADC10CTL0|=ENC;
 }
 
@@ -27,16 +27,18 @@ void delay()
 main()
 {
 	init_adc();
-	P1DIR=0x1;
-	P1OUT=0x00;
+	P1DIR=0x41;
+//	P1OUT=0x00;
 	while(1)
 	{
 		start();
 		while(busy());
-		if(ADC10MEM<380)
+		if(ADC10MEM<420)
 			P1OUT=0x1;
+		else if(ADC10MEM<440)
+			P1OUT=0x40;
 		else
-			P1OUT=0x00;
+			P1OUT=0x41;
 		delay();
 	}
 }
